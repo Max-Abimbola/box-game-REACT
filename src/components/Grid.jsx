ec6c68ef0527c40e06d7ccf37716391816c09afe
Max Abimbola <max.abimbola@gmail.com>
2023-06-20 16:10:18 +0100

    feat: Create grid component. Renders by mapping 2d array. Boxes change colour onClick.

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -1,26 +1,42 @@
 import { useState } from 'react'
+import '../styles/Grid.css'
 
 export default function Grid(props){
     const [gridArray, setGridArray] = useState([
-        [0,0,0],
-        [0,0,0],
-        [0,0,0]
+        [1,0,1],
+        [0,1,1],
+        [1,0,1]
     ])
 
+    const handleClick = (e) => {
+        e.preventDefault()
+
+        let row = parseInt(e.target.getAttribute('row'))
+        let col = parseInt(e.target.getAttribute('col'))
+
+        let gridArrayCopy = [...gridArray]
+
+        gridArray[row][col] === 1 ? gridArrayCopy[row][col] = 0 : gridArrayCopy[row][col] = 1
+
+        setGridArray(gridArrayCopy)
+    }
     return (
 
         <>
             <div className='grid-container'>
                 {
                     gridArray.map((row, rowIndex) => (
-                        <div key={rowIndex}>
-                            {row.map((col, colIndex) => (
+                            row.map((col, colIndex) => (
                                 <div
+                                className="grid-box"
+                                row={rowIndex}
+                                col={colIndex}
                                 key={`${rowIndex}-${colIndex}`}
+                                style={gridArray[rowIndex][colIndex] === 1 ? {backgroundColor:'#3C3AAB'} : {backgroundColor:'white'} }
+                                onClick={handleClick}
                                 >
-                                This will be a box</div>
-                            ))}
-                        </div>
+                                </div>
+                            ))
                     ))
                 }
             </div>
