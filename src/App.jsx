6484ecbc05816e9dcf0fdeda6c49c48af79f4033
Max Abimbola <max.abimbola@gmail.com>
2023-06-24 21:01:36 +0100

    feat: add feature to generate new grid when user matches pattern in time

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './styles/App.css'
 import Game from './components/Game'
 
 function App() {
   const [stageArray, setStageArray] = useState([
     [
-        [1,0,1],
-        [0,1,1],
-        [1,0,1]
+        [1,0,0],
+        [0,0,0],
+        [0,0,0]
     ],
     [
         [0,0,1],
@@ -19,15 +19,21 @@ function App() {
         [0,1,0],
         [0,0,0]
     ]
-])
+  ])
 
-const [stageNumber, setStageNumber] = useState(0)
+  const [stageNumber, setStageNumber] = useState(0)
 
-  return (
-    <>
-      <Game stageArray={stageArray}/>
-    </>
-  )
+  const [showComponent, setShowComponent] = useState(true)
+
+  const isWon = () => {
+    setStageNumber(stageNumber + 1)
+  }
+
+    return (
+      <>
+        {showComponent && <Game isWon={isWon} stageGrid={stageArray[stageNumber]} key={stageNumber}/>}
+      </>
+    )
 }
 
 export default App
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -3,89 +3,100 @@ import DrawingGrid from './DrawingGrid.jsx'
 import DisplayGrid from './DisplayGrid.jsx'
 import '../styles/Game.css'
 
-export default function Game(){
+export default function Game(props){
+
+    const initialDrawingGrid = [
+            [0,0,0],
+            [0,0,0],
+            [0,0,0]
+      ];
 
     const [stageNumber, setStageNumber] = useState(0)
-
-    const [drawingGrid, setDrawingGrid] = useState([
-        [0,0,0],
-        [0,0,0],
-        [0,0,0]
-
-    ]);
-
-    const [displayGrid, setDisplayGrid] = useState([
-        [1,0,1],
-        [0,1,1],
-        [1,0,1]
-    ])
-
-    const [gameState, setGamestate] = useState('isRunning')
+    const [drawingGrid, setDrawingGrid] = useState(initialDrawingGrid);
+    const [displayGrid, setDisplayGrid] = useState(props.stageGrid)
+    const [gameState, setGameState] = useState('isRunning')
     const [timerIsActive, setTimerIsActive] = useState(true)
     const [timerId, setTimerId] = useState(null)
 
-    const restGame = () => {
-        
-    }
-
     useEffect(() => {
-        
-        if(timerIsActive){
-            var timer = setTimeout(() => {
-                console.log('times up!')
-                setTimerIsActive(false)
-            },3000)
-            setTimerId(timer)
-        }
-
-        if((JSON.stringify(drawingGrid) === JSON.stringify(displayGrid)) && timerIsActive === true){
-            setGamestate('isWon')
-
-            const body = document.getElementById('body')
-            body.style.backgroundColor = '#65BF63'
-
-            const highlghtedBoxes = document.querySelectorAll('.grid-box')
-
-            highlghtedBoxes.forEach((box)=>{
+        if (timerIsActive) {
+          const intervalId = setInterval(() => {
+            clearInterval(intervalId);
+    
+            if (
+              JSON.stringify(drawingGrid) !== JSON.stringify(displayGrid) &&
+              timerIsActive === true
+            ) {
+              setGameState('isLost');
+    
+              const body = document.getElementById('body');
+              body.style.backgroundColor = '#AE6767';
+    
+              const highlightedBoxes = document.querySelectorAll('.grid-box');
+    
+              highlightedBoxes.forEach((box) => {
                 box.style.pointerEvents = 'none';
-                if(box.style.backgroundColor === 'rgb(60, 58, 171)'){
-                    box.style.backgroundColor = 'green'
+                if (box.style.backgroundColor === 'rgb(60, 58, 171)') {
+                  box.style.backgroundColor = '#962525';
                 }
-            })
-
-            setTimerIsActive(false)
-            
+              });
+    
+              console.log('YOU LOSE');
+            }
+          }, 5000);
+          setTimerId(intervalId);
         }
-
-        else if((JSON.stringify(drawingGrid) !== JSON.stringify(displayGrid)) && timerIsActive === false){
-            setGamestate('isLost')
-
-            const body = document.getElementById('body')
-            body.style.backgroundColor = '#AE6767'
-
-            const highlghtedBoxes = document.querySelectorAll('.grid-box')
-
-            highlghtedBoxes.forEach((box)=>{
-                box.style.pointerEvents = 'none';
-                if(box.style.backgroundColor === 'rgb(60, 58, 171)'){
-                    box.style.backgroundColor = '#962525'
-                }
-            })
-        }
-        
+    
         return () => {
-            clearTimeout(timerId)
+          clearInterval(timerId);
+        };
+      }, [timerIsActive, drawingGrid, displayGrid]);
+    
+    useEffect(() => {
+    if (
+        JSON.stringify(drawingGrid) === JSON.stringify(displayGrid) &&
+        timerIsActive === true
+    ) {
+        setGameState('isWon');
+
+        props.isWon()
+
+        const body = document.getElementById('body');
+        body.style.backgroundColor = '#65BF63';
+
+        const highlightedBoxes = document.querySelectorAll('.grid-box');
+
+        highlightedBoxes.forEach((box) => {
+        box.style.pointerEvents = 'none';
+        if (box.style.backgroundColor === 'rgb(60, 58, 171)') {
+            box.style.backgroundColor = 'green';
         }
+        });
 
-    }, [drawingGrid, displayGrid, timerIsActive])
+        console.log('YOU WIN');
+
+    }
+    }, [drawingGrid, displayGrid, timerIsActive]);
 
 
-    return (
+      return (
         <>
-            <div className={gameState === 'isWon' ? 'winning-screen' : gameState === 'isLost' ? 'losing-screen' : null}>
-                <DisplayGrid gridMatrix={displayGrid}/>
-                <DrawingGrid gridMatrix={drawingGrid} setDrawingGrid={setDrawingGrid}/>
-            </div>
+          <div
+            className={
+              gameState === 'isWon'
+                ? 'winning-screen'
+                : gameState === 'isLost'
+                ? 'losing-screen'
+                : null
+            }
+          >
+            <DisplayGrid gridMatrix={displayGrid} />
+            <DrawingGrid
+              gridMatrix={drawingGrid}
+              setDrawingGrid={setDrawingGrid}
+              setTimerIsActive={setTimerIsActive}
+            />
+          </div>
         </>
-    )
-}
+      );
+    }
