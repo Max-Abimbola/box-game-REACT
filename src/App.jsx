11af08f3ec49b2bf7f775a48d68954111633c89a
Max Abimbola <max.abimbola@gmail.com>
2023-06-27 16:55:55 +0100

    test: redeploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,51 @@
-import { useState, useEffect } from 'react'
-import './styles/App.css'
-import Game from './components/Game'
-/* import playSound from './helpers/playSound.jsx' */
-import randomString from './helpers/randomString.js'
-
-
-/* import soundFile from './assets/correct-answer-sound-1.wav' */
-
+import { useState, useEffect } from 'react';
+import './styles/App.css';
+import Game from './components/Game';
+import randomString from './helpers/randomString.js';
 
 function App() {
-
-/*   const [stageArray, setStageArray] = useState(randomString(1,null,null)) */
-
-  const [stageNumber, setStageNumber] = useState(0)
-
-  const [showComponent, setShowComponent] = useState(true)
-  
-  const [score, setScore] = useState(0)
-
-  const [soundEnabled, setSoundEnabled] = useState(true)
+  const [stageNumber, setStageNumber] = useState(0);
+  const [showComponent, setShowComponent] = useState(true);
+  const [score, setScore] = useState(0);
+  const [soundEnabled, setSoundEnabled] = useState(true);
 
   const isWon = () => {
-    setStageNumber(stageNumber + 1)
-    const body = document.getElementById('body');
+    setStageNumber(stageNumber + 1);
+  };
 
-  }
   const toggleSound = () => {
-    setSoundEnabled(!soundEnabled)
-  }
+    setSoundEnabled(!soundEnabled);
+  };
 
   const incrementScore = () => {
-    setScore(score + 1)
-  }
+    setScore(score + 1);
+  };
 
+  const initializeAudioContext = () => {
+    const audioContext = new AudioContext();
+    // Use the audioContext as needed
+  };
 
+  useEffect(() => {
+    // Initialize the audio context after the first user gesture
+    initializeAudioContext();
+  }, []);
 
-    return (
-      <>
-        <button id='toggle-sound-button' onClick={toggleSound}>{soundEnabled ? 'Sound Enabled' : ' Sound Disabled'}</button>
-        <Game soundEnabled={soundEnabled} isWon={isWon} stageGrid={[[1,0,0],[0,0,0],[0,0,0]]/* randomString(1,null,null) */} key={stageNumber} score={score} incrementScore={incrementScore}/>
-        
-      </>
-    )
+  return (
+    <>
+      <button id="toggle-sound-button" onClick={toggleSound}>
+        {soundEnabled ? 'Sound Enabled' : 'Sound Disabled'}
+      </button>
+      <Game
+        soundEnabled={soundEnabled}
+        isWon={isWon}
+        stageGrid={[[1, 0, 0], [0, 0, 0], [0, 0, 0]]}
+        key={stageNumber}
+        score={score}
+        incrementScore={incrementScore}
+      />
+    </>
+  );
 }
 
-export default App
+export default App;
